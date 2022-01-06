87ff28961cf59f4ef31582d919cddaf06fb5bd1a
Muhammad Hamza Arif <mhamza.arif@salesassist.pk>
2022-01-06 15:57:16 +0500

    default context menu setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { sampleData } from './datasource';
+import { ToolbarItems } from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TreeTable';
+  public data: Object[] = [];
+  public pager: Object = {};
+  public editSettings: Object = {};
+  public contextMenuItems: Object[] = [];
+  ngOnInit(): void {
+    this.editSettings = {
+      allowEditing: true,
+      allowAdding: true,
+      allowDeleting: true,
+      mode: 'Row',
+    };
+    this.contextMenuItems = [
+      'AutoFit',
+      'AutoFitAll',
+      'SortAscending',
+      'SortDescending',
+      'Edit',
+      'Delete',
+      'Save',
+      'Cancel',
+      'PdfExport',
+      'ExcelExport',
+      'CsvExport',
+      'FirstPage',
+      'PrevPage',
+      'LastPage',
+      'NextPage',
+    ];
+
+    this.pager = { pageSize: 8 };
+    this.data = sampleData;
+    console.log(this.data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import {
+  PageService,
+  FilterService,
+  EditService,
+  ToolbarService,
+} from '@syncfusion/ej2-angular-treegrid';
+import {
+  SortService,
+  ResizeService,
+  ExcelExportService,
+  PdfExportService,
+  ContextMenuService,
+} from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, TreeGridModule, ButtonModule, DropDownListAllModule],
+  providers: [
+    PageService,
+    SortService,
+    FilterService,
+    EditService,
+    SortService,
+    ResizeService,
+    ExcelExportService,
+    PdfExportService,
+    ContextMenuService,
+    ToolbarService,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
