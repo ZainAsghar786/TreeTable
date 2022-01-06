5210e07cac8e036da764f98bbe5761f4e84b7a78
Muhammad Hamza Arif <mhamza.arif@salesassist.pk>
2022-01-06 11:07:56 +0500

    initialization...scss change to css

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tree-grid'),
+      dir: require('path').join(__dirname, './coverage/TreeTable'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tree-grid'`, () => {
+  it(`should have as title 'TreeTable'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tree-grid');
+    expect(app.title).toEqual('TreeTable');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tree-grid app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TreeTable app is running!');
   });
 });
